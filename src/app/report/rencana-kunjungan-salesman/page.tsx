fad610938fe4a58d654e4c74a9bb030d0731df63
Gede Dewo Wahyu M.W <programmer3.it@kcngroup.co.id>
2026-06-15 15:27:11 +0700

    refactor: 💡 rks report

---
+++ src/app/report/rencana-kunjungan-salesman/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useSearchParams } from "next/navigation";
+import { FC, ReactElement, Suspense } from "react";
+
+import { IViewerWrapper } from "@/src/components/templates/viewer-wrapper";
+const ViewerWrapper = dynamic<IViewerWrapper>(async () => (await import("@/src/components/templates/viewer-wrapper")).default, { ssr: false });
+
+const ReportContent: FC = (): ReactElement => {
+  const searchParams = useSearchParams();
+  const entitasParam = searchParams.get("entitas");
+  const tokenParam = searchParams.get("token");
+  const visiblePrintParam = searchParams.get("visiblePrint");
+
+  const params = Array.from({ length: 2 }, (_, i) => {
+    const paramValue = searchParams.get(`param${i + 1}`);
+    return paramValue ? { Name: `param${i + 1}`, Value: [paramValue] } : null;
+  }).filter(Boolean);
+
+  const parameter = {
+    ReportParams: [
+      entitasParam ? { Name: "entitas", Value: [entitasParam] } : null,
+      tokenParam ? { Name: "token", Value: [tokenParam] } : null,
+      ...params,
+    ].filter(Boolean),
+  };
+
+  const visiblePrint = visiblePrintParam === "false" ? false : true;
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <ViewerWrapper reportParam={parameter} reportUri="/assets/report/rencana-kunjungan-salesman/rks.rdlx-json" visiblePrint={visiblePrint} />
+    </div>
+  );
+};
+
+const ReportPage: FC = (): ReactElement => (
+  <Suspense fallback={<div className="flex h-screen w-full items-center justify-center">Loading...</div>}>
+    <ReportContent />
+  </Suspense>
+);
+
+export default ReportPage;
